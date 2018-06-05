20d1537b4a2cbe04379e4e36d516a0d87d850cd9
Przemysław Sadownik <przemek.sadownik@gmail.com>
2018-06-05 16:24:19 +0200

    ps360958_6 added, modified script for new models

--- make_new.sh
+++ make_new.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euox pipefail
-cp models/ps360958_4.py models/ps360958_$1.py
-cat configs/ps360958_4.json | sed "s/ps360958_4/ps360958_$1/g" > configs/ps360958_$1.json
-cat mains/ps360958_main4.py | sed "s/ps360958_4/ps360958_$1/g" > mains/ps360958_main$1.py
+SRC=${2:-"4"}
+
+cp models/ps360958_$SRC.py models/ps360958_$1.py
+cat configs/ps360958_$SRC.json | sed "s/ps360958_$SRC/ps360958_$1/g" > configs/ps360958_$1.json
+cat mains/ps360958_main$SRC.py | sed "s/ps360958_$SRC/ps360958_$1/g" > mains/ps360958_main$1.py
 
 
