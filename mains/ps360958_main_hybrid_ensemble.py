35486d3a92d0e6081908e767481c39659c1953da
Przemysław Sadownik <przemek.sadownik@gmail.com>
2018-06-10 08:55:50 +0200

    hybrid model final

--- mains/ps360958_main_hybrid_ensemble.py
+++ mains/ps360958_main_hybrid_ensemble.py
@@ -33,18 +33,18 @@ def main():
     logger = Logger(sess, config)
     # create trainer and pass all the previous components to it
     trainer = SimpleTrainer(sess, model, data, config, logger)
-    saverExternal = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='external'))
-    saverExternal.restore(sess, "experiments/model_8/model8.ckpt_2")
+    #saverExternal = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='external'))
+    #saverExternal.restore(sess, "experiments/model_8/model8.ckpt_2")
     #saver5 = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='ps360958_5_part'))
     #saver5.restore(sess, "experiments/ps360958_5_part/checkpoint/-2320")
-    saver5 = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='ps360958_5_part'))
-    saver5.restore(sess, "experiments/ps360958_5_part_backup/checkpoint/-1600")
-    saver6 = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='ps360958_6_part'))
-    saver6.restore(sess, "experiments/ps360958_6_part/checkpoint/-2400")
-    saver8 = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='ps360958_8_part'))
-    saver8.restore(sess, "experiments/ps360958_8_part/checkpoint/-5600")
+    #saver5 = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='ps360958_5_part'))
+    #saver5.restore(sess, "experiments/ps360958_5_part_backup/checkpoint/-1600")
+    #saver6 = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='ps360958_6_part'))
+    #saver6.restore(sess, "experiments/ps360958_6_part/checkpoint/-2400")
+    #saver8 = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='ps360958_8_part'))
+    #saver8.restore(sess, "experiments/ps360958_8_part/checkpoint/-5600")
     # here you train your model
-    trainer.train()
+    #trainer.train()
     trainer.validate()
 
 
